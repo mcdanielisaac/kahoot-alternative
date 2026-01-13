afeba6d520f37e5f7f1436178be36f7fdfbeb958
Isaac McDaniel <isaac@zone6.online>
2026-01-13 01:21:31 -0600

    margin on register btn

--- src/app/game/[id]/page.tsx
+++ src/app/game/[id]/page.tsx
@@ -131,7 +131,23 @@ function Results({ participant }: { participant: Participant }) {
         <p>Thanks for playing ⭐️</p>
         <a
           href="https://oa-g2.org/register"
-          className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 dark:bg-indigo-500 dark:shadow-none dark:hover:bg-indigo-400 dark:focus-visible:outline-indigo-500"
+          className="rounded-md
+          bg-indigo-600
+          mt-6
+          px-3.5
+          py-2.5
+          text-sm
+          font-semibold
+          text-white
+          shadow-xs
+          hover:bg-indigo-500
+          focus-visible:outline-2
+          focus-visible:outline-offset-2
+          focus-visible:outline-indigo-600
+          dark:bg-indigo-500
+          dark:shadow-none
+          dark:hover:bg-indigo-400
+          dark:focus-visible:outline-indigo-500"
           >Register for the Lone Star Fellowship</a>
       </div>
     </div>
